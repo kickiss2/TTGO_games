67ffd346fe8291861eb9fceb7570627353f9beb1
Saulius Maknickas <saulius.maknickas@bluebridge.lt>
2024-02-10 21:30:30 +0200

    Inertia for paddle

--- TFT_Squash/TFT_Squash.ino
+++ TFT_Squash/TFT_Squash.ino
@@ -11,6 +11,7 @@ int16_t w = 235;
 int16_t lbut = 0;
 int16_t rbut = 35;
 
+int16_t ipady = 0;
 int16_t pady = 0;
 int16_t padh = 20;
 
@@ -21,6 +22,9 @@ int16_t x = 0;
 int16_t y = 0;
 int16_t dx = 1;
 int16_t dy = 1;
+
+int16_t inertia = 0;
+int16_t ispeed = 130;
 
 void setup(void) {
   tft.init();
@@ -34,15 +38,15 @@ void setup(void) {
 
 void loop() {
   delay(dly);
-  ball();
   paddle();
+  ball();
 }
 
 void resetBall() {
   x = 0;
-  y = w/2;
+  y = random(0, h);
   dx = 1;
-  dy = 1;
+  dy = random(1, 3) - 1;
 }
 
 void ball() {
@@ -67,22 +71,35 @@ void ball() {
     if (y >= pady && y <= pady + padh) {
       dx = -dx;
     }
-    if (y <= pady + padh && y >= (pady + padh/2) && dy < 0) {
-      dy = -dy;
+    if (y <= pady + padh && y >= (pady + padh / 2) && dy <= 0) {
+      dy = 1;
     }
-    if (y >= pady && y <= (pady + padh/2) && dy > 0) {
-      dy = -dy;
+    if (y >= pady && y <= (pady + padh / 2) && dy >= 0) {
+      dy = -1;
     }
   }
 }
 
 void paddle() {
   tft.drawRect(w - 4, pady, 4, padh, BLACK);
-  if (digitalRead(lbut) == 0 and pady + padh < h) {
-    pady = pady + 1;
+  if (digitalRead(lbut) == 0) {
+    inertia = ispeed;
   }
-  if (digitalRead(rbut) == 0 and pady > 0) {
-    pady = pady - 1;
+  if (digitalRead(rbut) == 0) {
+    inertia = -ispeed;
+  }
+  if (digitalRead(lbut) == 0 && digitalRead(rbut) == 0) {
+    inertia = 0;
+  }
+  if (inertia > 0 and pady + padh < h) {
+    ipady = ipady + inertia;
+    pady = round(ipady/ispeed);
+    inertia = inertia - 1;
+  }
+  if (inertia < 0 and pady > 0) {
+    ipady = ipady + inertia;
+    pady = round(ipady/ispeed);
+    inertia = inertia + 1;
   }
   tft.drawRect(w - 4, pady, 4, padh, WHITE);
 }
